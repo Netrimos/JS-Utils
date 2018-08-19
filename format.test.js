c21920d25960c67b409fdc269bb9b00bdf0c59b5
Erik Thompson <xsanic@Palladium.local>
2018-08-19 02:55:41 -0400

    Test Updates

--- format.test.js
+++ format.test.js
@@ -14,8 +14,8 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-//console.log(utils.timestamp());
-//console.log(utils.createGUID());
+console.log(utils.timestamp());
+console.log(utils.createGUID());
 console.log(utils.toTimestamp("08/18/2018 15:05:53"));
 console.log();
 console.log(utils.formatTime(utils.timestamp(), argv.type));
